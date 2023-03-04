1868b867919656fe162a06e80e8ec33da0e9ab61
Luke Patterson <luke.patterson@icloud.com>
2023-03-04 15:10:37 -0600

    Made the wingChord variable an optional input to the constructor.

--- AerodynamicsCPP/src/NACA4.h
+++ AerodynamicsCPP/src/NACA4.h
@@ -5,12 +5,12 @@
 #ifndef NACA4_H
 #define NACA4_H
 
-class NACA4
+class NACA4 : Airfoil
 {
 
 public:
 	void NACA4();
-	void NACA4(std::string NACA, double wingChord, double oswaldFactor_e = 0.7);
+	void NACA4(std::string NACA, double wingChord = 1, double oswaldFactor_e = 0.7);
 
 	double get_a0l();
 	double getCmac();
