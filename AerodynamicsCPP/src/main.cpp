ef89fec123d924f7fb0b9ff2b8ef0ea05ed48f84
Luke Patterson <luke.patterson@icloud.com>
2023-03-04 15:12:00 -0600

    Removed printing each data point to the output.

--- AerodynamicsCPP/src/main.cpp
+++ AerodynamicsCPP/src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv)
 	for (size_t i{}; i < x.size(); i++)
 	{
 		y.push_back(sin(x.at(i)));
-		cout << y.at(i) << endl;
+//		cout << y.at(i) << endl;
 	}
 
 	printf("The integral from 0 to pi/2 of sinx is: %.2f\n", IntegrateSimpson(y,(double) 0, (double) M_PI/2));
