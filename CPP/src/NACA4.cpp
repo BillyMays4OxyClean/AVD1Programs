affc01dc44d2d598e769f4fd0283e606b629eddc
Luke Patterson <lcp.patterson@gmail.com>
2023-04-21 01:26:56 -0500

    Added camber line functions.

--- CPP/src/NACA4.cpp
+++ CPP/src/NACA4.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <string>
 
 #include "NACA4.h"
 
@@ -9,7 +8,7 @@ using namespace std;
 NACA4::NACA4(std::string NACA,int nseg=128)
 :
 c(1),
-m_x(LinSpace(0,c,nseg))
+x(LinSpace(0,c,nseg))
 {
 	/* Make a NACA string validator, based on a regex maybe. If it doesn't
 	 pass the validator, an exception is made. */
@@ -40,18 +39,26 @@ NACA4::NACA4(const char* NACA,int nseg=128)
       	cout << t << endl;
 };
 
-void
-NACA4::createAbscissa()
-{
-	m_x = LinSpace(0,c,nseg);
-}
-
 vector<double>
 NACA4::calculateCamberLine()
 {
-	for (i = 0; i < (nseg-1); i++)
+      /* Note to self: This c++ loop differs from its matlab counterpart in the sense that "i"
+         i does not represent x in this case. It represents the current index of x.
+         Therefore, another variable for x must be created. */
+      
+	for (size_t i = 0; i < (nseg-1); i++)
 	{
-
+            double xi = x.at(i);
+            if (i <= p)
+            {
+                  yc.at(i) = m / pow(p,2) * (2*p*xi - pow(xi,2));
+                  dycdx.at(i) = 2 * m / pow(p, 2.0) * (p - xi);
+            }
+            else if (xi > p)
+            {
+                  yc.at(i) = m / pow((1-p), 2.0) * ((1 - 2*p) + 2*p*xi - pow(xi,2));
+                  dycdx.at(i) = 2 * m / pow((1-p), 2.0) * (p - xi);
+            }
 	}
 }
 
